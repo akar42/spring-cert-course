b4528a2a8aacb8c581e27f8d852fc38f42179f16
akar42 <timofei.tauszanow@gmail.com>
2025-07-18 16:47:11 +0200

    Annotation-based Configuration Lab
    - Submodule 16-annotations
    - RewardsConfig is now scanning for components
    - Used appropriate stereotypes for classes
    - Constructor and setter injection
    - Lifecycle methods for JdbcRestaurantRepository
    - Tests from previous lab

--- lab/16-annotations/src/main/java/rewards/internal/RewardNetworkImpl.java
+++ lab/16-annotations/src/main/java/rewards/internal/RewardNetworkImpl.java
@@ -10,6 +10,9 @@ import rewards.internal.restaurant.Restaurant;
 import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.RewardRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import common.money.MonetaryAmount;
 
 /**
@@ -30,7 +33,7 @@ import common.money.MonetaryAmount;
  * - Inject all 3 dependencies.  Decide if you should use field
  *   injection or constructor injection.
  */
-
+@Component
 public class RewardNetworkImpl implements RewardNetwork {
 
 	private AccountRepository accountRepository;
@@ -45,7 +48,7 @@ public class RewardNetworkImpl implements RewardNetwork {
 	 * @param restaurantRepository the repository for loading restaurants that determine how much to reward
 	 * @param rewardRepository the repository for recording a record of successful reward transactions
 	 */
-	
+	@Autowired
 	public RewardNetworkImpl(AccountRepository accountRepository, RestaurantRepository restaurantRepository,
 			RewardRepository rewardRepository) {
 		this.accountRepository = accountRepository;
