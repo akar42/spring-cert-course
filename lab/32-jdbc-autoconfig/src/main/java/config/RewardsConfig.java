1d3889c66e197bad676fb0cb65235ca3ed8a255c
akar42 <timofei.tauszanow@gmail.com>
2025-07-26 17:01:32 +0200

    SpringBoot Introduction Lab
    - Submodule 32-jdbc-autoconfig
    - Inspected SpringBoot auto-configuration
        - Explicit defenition of beans
        - Excuded DataSource auto-configuration
    - Command Line Runners to validate
    - Configuration properties

--- lab/32-jdbc-autoconfig/src/main/java/config/RewardsConfig.java
+++ lab/32-jdbc-autoconfig/src/main/java/config/RewardsConfig.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
 import rewards.RewardNetwork;
 import rewards.internal.RewardNetworkImpl;
 import rewards.internal.account.AccountRepository;
@@ -21,12 +23,12 @@ public class RewardsConfig {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	DataSource dataSource;
+	// DataSource dataSource;
 
-	@Autowired  // This @Autowired annotation is optional here
-	public RewardsConfig(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
+	// @Autowired  // This @Autowired annotation is optional here
+	// public RewardsConfig(DataSource dataSource) {
+	// 	this.dataSource = dataSource;
+	// }
 
     // TODO-10 (Optional) : Switch back to explicit `DataSource` configuration
     // (Instead of using auto-configured DataSource, we are going to configure
@@ -37,7 +39,7 @@ public class RewardsConfig {
     // - Uncomment @Bean method below
     // - Remove the code above that performs DataSource injection
     // - Fix compile errors in this code
-    /*
+    
     @Bean
     public DataSource dataSource() {
         logger.debug("Creating the datasource bean explicitly");
@@ -48,30 +50,30 @@ public class RewardsConfig {
                         .addScript("classpath:data.sql")
                         .build();
     }
-    */
+    
 
     @Bean
-    public RewardNetwork rewardNetwork() {
+    public RewardNetwork rewardNetwork(DataSource dataSource) {
         return new RewardNetworkImpl(
-                accountRepository(),
-                restaurantRepository(),
-                rewardRepository());
+                accountRepository(dataSource),
+                restaurantRepository(dataSource),
+                rewardRepository(dataSource));
     }
 
     @Bean
-    public AccountRepository accountRepository() {
+    public AccountRepository accountRepository(DataSource dataSource) {
         JdbcAccountRepository repository = new JdbcAccountRepository(dataSource);
         return repository;
     }
 
     @Bean
-    public RestaurantRepository restaurantRepository() {
+    public RestaurantRepository restaurantRepository(DataSource dataSource) {
         JdbcRestaurantRepository repository = new JdbcRestaurantRepository(dataSource);
         return repository;
     }
 
     @Bean
-    public RewardRepository rewardRepository() {
+    public RewardRepository rewardRepository(DataSource dataSource) {
         JdbcRewardRepository repository = new JdbcRewardRepository(dataSource);
         return repository;
     }
--- lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
+++ lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
@@ -2,7 +2,15 @@ package rewards;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import config.RewardsConfig;
 
 // TODO-00 : In this lab, you are going to exercise the following:
 // - Understanding how auto-configuration is triggered in Spring Boot application
@@ -35,6 +43,8 @@ import org.springframework.boot.SpringApplication;
 // TODO-13 (Optional) : Follow the instruction in the lab document.
 //           The section titled "Build and Run using Command Line tools".
 
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@Import(RewardsConfig.class)
 public class RewardsApplication {
     static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
 
@@ -56,7 +66,17 @@ public class RewardsApplication {
     // - Use the JdbcTemplate bean that Spring Boot auto-configured for you
     // - Run this application and verify "Hello, there are 21 accounts" log message
     //   gets displayed in the console
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate) {
+        return args -> logger.info("Hello there. " 
+            + jdbcTemplate.queryForObject(SQL, Long.class) 
+            + " accounts in DB");
+    }
 
+    @Bean
+    CommandLineRunner commandLineRunner2(RewardsRecipientProperties rewardsRecipientProperties) {
+        return args -> logger.info(rewardsRecipientProperties.toString());
+    }
     // TODO-07 (Optional): Enable full debugging in order to observe how Spring Boot
     //           performs its auto-configuration logic
     // - Follow TO-DO-07 in application.properties, then come back here.
